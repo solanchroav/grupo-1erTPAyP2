982d6fa624199110802e5155cdc95344ed014eb5
faber <faber@DESKTOP-216U5VM>
2020-05-24 20:56:38 -0300

    Cajero con el metodo Main, donde se inicia y finaliza el programa

---
+++ Simulador ATM/src/Cajero.java
@@ -0,0 +1,13 @@
+
+public class Cajero {
+	
+	private static int billetesDeCien = 500;
+	private static int billetesDeQuinientos = 500;
+	private static int billetesDeMil = 500;
+	
+	
+	public static void main(String[] args) {
+
+	}
+
+}
